700f3a2d78107b767bc9062cef651f863a735696
prismgentem <foksfire12345px@gmail.com>
2024-10-06 09:28:56 +0300

    init SpringBoot

---
+++ src/test/java/org/example/smartresidenceactivity/SmartResidenceActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartresidenceactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartResidenceActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
