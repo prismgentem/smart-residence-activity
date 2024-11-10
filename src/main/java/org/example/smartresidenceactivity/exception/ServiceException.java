93048764d415384f875afa04f448deb66c4c2444
prismgentem <foksfire12345px@gmail.com>
2024-11-10 22:59:46 +0300

    кейклок с тестовым юзером и админом убрал хидеры)

--- src/main/java/org/example/smartresidenceactivity/exception/ServiceException.java
+++ src/main/java/org/example/smartresidenceactivity/exception/ServiceException.java
@@ -11,14 +11,6 @@ import java.util.Map;
 public class ServiceException extends RuntimeException {
     private final Integer status;
     private final String code;
-    private final Map<String, String> headers = new HashMap<>();
-
-    public ServiceException(ErrorType errorType, String message, Object... args) {
-        super(String.format(message, args));
-        this.status = errorType.getStatus();
-        this.code = errorType.getCode();
-    }
-
     public ServiceException(ErrorType errorType, String message) {
         super(message);
         this.status = errorType.getStatus();
